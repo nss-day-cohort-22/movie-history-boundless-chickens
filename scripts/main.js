2a6c62e537d57b8d6cba503e8645586e6fed6bf4
Jenna Solis <jennabsol@gmail.com>
2017-11-29 20:46:54 -0600

    Materialize css

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,8 @@
 const auth = require("./auth/auth")
 const newMoviesEvent = require("./movieAPI/findMoviesSection")
 const displayMoviesEventListener = require("./displayMovies/displayMoviesSection")
-
+const $ = require("jquery")
+require("materialize")
 auth.init()
 auth.register()
 auth.logout()
